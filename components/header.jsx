d39d43b12d194e8f83f15e0cb29d9a48b65b0d16
Anthony DiBernardo <adibern2@charlotte.edu>
2025-10-29 14:16:36 -0400

    removed about

--- components/header.jsx
+++ components/header.jsx
@@ -7,7 +7,6 @@ function Header(){
                 <h1>Team 3's Blog</h1>
                 <ul>
                     <li><a href="/">home</a></li>
-                    <li><a href="/about">about</a></li>
                     <li><a href="/contact">contact</a></li>
                 </ul>
         </header>
--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import BlogPost from './components/App.jsx'
+import BlogPost from './components/Posts.jsx'
 import Header from './components/header.jsx'
 import Footer from './components/footer.jsx'
 
