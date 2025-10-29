4d2b12dafa8ad6ded9c783c5e349cffbd716ffb2
Anthony DiBernardo <adibern2@charlotte.edu>
2025-10-29 14:09:57 -0400

    Created lab 7 and unified comment and post

--- main.jsx
+++ main.jsx
@@ -1,18 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Post from './components/App.jsx'
+import BlogPost from './components/App.jsx'
 import Header from './components/header.jsx'
 import Footer from './components/footer.jsx'
-import Comments from './components/comments.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
-    <Post />
-    <Comments />
-  
+    <BlogPost />
     <Footer />
   </StrictMode>,
 )
