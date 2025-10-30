b8178f2a0cc4910305f237af1e0910a016c5673b
Andrew <ahooker9@charlotte.edu>
2025-10-30 11:53:29 -0400

    comments and posts are two seperate files now

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -8,16 +8,6 @@ function Post() {
             <p id="author">Anthony DiBernardo</p>
             <p id="date">Thu Oct 23 2025 15:53:00</p>
             <p id="body">Cras ipsum quis praesent congue malesuada, nunc in, sed aenean nec id. Praesent, eget in ac convallis pretium, dapibus sem praesent in volutpat suspendisse. Ultricies pellentesque, lacus dignissim, sit congue euismod risus imperdiet ac hac dignissim. Eros, faucibus, faucibus consequat proin suspendisse purus convallis elementum et ligula at. Ac at consequat purus id, lacus, ante tellus facilisis nibh montes tortor. Vel, tempor, diam ipsum aliquam montes lectus sit dapibus eget at ultricies.</p>
-            <form action="/submit-comment" method="POST">
-                <textarea name="comment" placeholder="Write your comment here..." required></textarea>
-                <button type="submit">Submit</button>
-            </form>
-            <h3>Existing Comments</h3>
-            <ul>
-                <li class="Comment">comment 1</li>
-                <li class="Comment">comment 2</li>
-                <li class="Comment">comment 3</li>
-            </ul>
         </main>
     );
 }
