abc826e3f9a9e6090ca8b05caee6def257ee5585
Anthony DiBernardo <adibern2@charlotte.edu>
2025-11-05 12:56:22 -0500

    removed archive and attempted other fixes

--- main.jsx
+++ main.jsx
@@ -1,6 +1,7 @@
-import { StrictMode } from 'react'
+import { StrictMode, BrowserRouter, Routes, Route } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Router from './components/Router.jsx'
 import Header from './components/header.jsx'
 import Home from './components/Home.jsx'
 import Footer from './components/footer.jsx'
@@ -8,8 +9,11 @@ import Footer from './components/footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Router />
     <Header />
-    <Home />
+    <BrowserRouter>
+        <Home />
+    </BrowserRouter>
     <Footer />
   </StrictMode>,
 )
