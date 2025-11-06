86ab339f94d409fe097c017db95915b22febf219
Andrew <ahooker9@charlotte.edu>
2025-11-06 16:05:51 -0500

    post for merge

---
+++ components/Blog/IndividualPost.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Post from "./post";
+
+function IndividualPost({value}){
+
+    return(
+        <div>
+            <Post />
+        </div>
+    );
+}
+
+export default IndividualPost;
